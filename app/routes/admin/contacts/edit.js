30670479927ee83247087bcfbd9a6b3fedff0fe5
James Wright <jcwright04@gmail.com>
2016-03-07 21:07:09 -0800

    added edit functionality to invites and contacts

--- app/routes/admin/contacts/edit.js
+++ app/routes/admin/contacts/edit.js
@@ -3,5 +3,25 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model(params){
         return this.store.findRecord('contact', params.contact_id);
+    },
+    actions: {
+        updateContact(contactData){
+
+            contactData.save().then(() => this.transitionTo('admin.contacts.view'));
+        },
+        willTransition(transition){
+            let model = this.controller.get('model');
+
+            if(model.get('hasDirtyAttributes')){
+
+                let confirmation = confirm("Your changes haven't saved yet, would you like to leave this from?");
+
+                if(confirmation){
+                    model.rollbackAttributes();
+                }else {
+                    transition.abort();
+                }
+            }
+        }
     }
 });
--- app/routes/admin/invitations/edit.js
+++ app/routes/admin/invitations/edit.js
@@ -3,5 +3,25 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model(params){
         return this.store.findRecord('invitation', params.invitation_id)
+    },
+    actions : {
+        updateInvite(inviteEmail){
+            inviteEmail.save().then(() => this.transitionTo('admin.invitations.view'))
+        },
+
+        willTransition(transition){
+            let model = this.controller.get('model');
+
+            if (model.get('hasDirtyAttributes')) {
+                let confirmation = confirm("Your changes won't be saved!");
+
+                if (confirmation) {
+                    model.rollbackAttributes();
+                }
+                else {
+                    transition.abort();
+                }
+            }
+        }
     }
 });
